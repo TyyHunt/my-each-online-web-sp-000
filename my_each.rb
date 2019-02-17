4fc8b1eba3b1140b747559b179451cbfd8f7b29f
Tyler Hunt <tyhunt04@gmail.com>
2019-02-17 05:50:14 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,12 +2,12 @@ def my_each(array)# put argument(s) here
   if block_given?
     i = 0
 
-    while i < array.length do |word|
+    while i < array.length
       puts word
     end
 
     array
-  else
+  else 
     puts "Hey! There's no block given."
   end
 end
